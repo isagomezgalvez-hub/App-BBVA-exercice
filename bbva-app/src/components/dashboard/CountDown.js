333d05adaf7ee25b79351e180ad363e7ccbfed1c
“GuardianesdelaNoche” <equipoguardianesdelanoche@gmail.com>
2021-10-01 15:25:25 +0200

    Displays the counter with the last date of access.

--- bbva-app/src/components/dashboard/CountDown.js
+++ bbva-app/src/components/dashboard/CountDown.js
@@ -4,19 +4,29 @@ import { firebase } from '../../api/firebase-config';
 let accessDate = firebase.auth().currentUser?.metadata.lastSignInTime
 const countDown = new Date(accessDate).getTime();
 
-export const CountDown = () => {
+export const CountDown = ({ timerDays, timerHours, timerMinutes, timerSeconds }) => {
+
+	
 	
 	return (
-			<div class="countdown">
+			<div className="countdown">
 			<ul>
-				<li><span id="days">00</span>Days</li>
-				<li><span id="hours">00</span>Hours</li>
-				<li><span id="minutes">00</span>Minutes</li>
-				<li><span id="seconds">00</span>Seconds</li>
+				<li><span id="days">{timerDays}</span>Days</li>
+				<li><span id="hours">{timerHours}</span>Hours</li>
+				<li><span id="minutes">{timerMinutes}</span>Minutes</li>
+				<li><span id="seconds">{timerSeconds}</span>Seconds</li>
 			</ul>
 			<p id="newUser"></p>
 
 		</div>
 		
 	)
-	}
+	};
+
+	CountDown.defaultProps = {
+		timerDays: 10,
+		timerHours:10,
+		timerMinutes:10,
+		timerSeconds:10
+	};
+	
--- bbva-app/src/components/dashboard/DashboardScreen.js
+++ bbva-app/src/components/dashboard/DashboardScreen.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { useDispatch } from 'react-redux';
-
+import { firebase } from '../../api/firebase-config';
 
 import { CountDown } from './CountDown';
 import { startLogout } from '../../actions/auth';
@@ -9,10 +9,58 @@ export const DashboardScreen = () => {
 	const dispatch = useDispatch()
 
 
+	const [timerDays, setTimerDays] = useState()
+	const [timerHours, setTimerHours] = useState()
+	const [timerMinutes, setTimerMinutes] = useState()
+	const [timerSeconds, setTimerSeconds] = useState()
+
+	// Date lastLogin
+	let loginAccessDate = firebase.auth().currentUser?.metadata.lastSignInTime
+	let countDownLogin = new Date(loginAccessDate).getTime();
+
 	const handleLogout = ()=>{
 		dispatch(startLogout())
 	}
 
+
+	let interval;
+
+	const startTimer=()=>{
+		
+		const countDownDate = new Date(countDownLogin).getTime();
+
+		interval = setInterval(()=>{
+			const now = new Date().getTime();
+			
+			const distance = now - countDownDate;
+
+			const second = 1000,
+				minute = second * 60,
+				hour = minute * 60,
+				day = hour * 24;
+
+			const days = Math.floor((distance / (day)));
+			const hours = Math.floor((distance % (day)) / (hour));
+			const minutes = Math.floor((distance % (hour)) / (minute));
+			const seconds = Math.floor((distance % (minute)) / second);
+
+
+			if (distance <= 0){
+				//Stop timer
+				clearInterval(interval.current)
+			}else{
+				//Update the timer
+				setTimerDays(days)
+				setTimerHours(hours)
+				setTimerMinutes(minutes)
+				setTimerSeconds(seconds)
+			}
+		})
+	}
+
+	useEffect(() => {
+		startTimer()
+	}, [])
 
 	return (
 		
@@ -21,7 +69,12 @@ export const DashboardScreen = () => {
 				<h1 className="dashboard-title ">Welcome!</h1>
 					<p className="dashboard__description">The last time you accessed was</p>
 					<div>
-						<CountDown/>
+						<CountDown 
+							timerDays={timerDays} 
+							timerHours={timerHours}
+							timerMinutes={timerMinutes}
+							timerSeconds={timerSeconds}
+						/>
 					</div>
 
 					<button 
