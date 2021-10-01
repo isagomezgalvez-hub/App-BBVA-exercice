eb50a31887037ce81cd446768c06088ca0127918
“GuardianesdelaNoche” <equipoguardianesdelanoche@gmail.com>
2021-10-01 14:32:26 +0200

    Get the last Login

--- bbva-app/src/components/dashboard/CountDown.js
+++ bbva-app/src/components/dashboard/CountDown.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react'
 import { firebase } from '../../api/firebase-config';
 
+let accessDate = firebase.auth().currentUser?.metadata.lastSignInTime
+const countDown = new Date(accessDate).getTime();
+
 export const CountDown = () => {
-	let lastLogin = firebase.auth().currentUser?.metadata.lastSignInTime;
 	
-	console.log('Ultimo Login', lastLogin)
 	return (
+			<div class="countdown">
+			<ul>
+				<li><span id="days">00</span>Days</li>
+				<li><span id="hours">00</span>Hours</li>
+				<li><span id="minutes">00</span>Minutes</li>
+				<li><span id="seconds">00</span>Seconds</li>
+			</ul>
+			<p id="newUser"></p>
+
+		</div>
 		
-			<div className="countdown">
-				<ul>
-					<li><span id="days">00</span> Days</li>
-					<li><span id="hours">00</span> Hours</li>
-					<li><span id="minutes">00</span> Minutes</li>
-					<li><span id="seconds">00</span> Seconds</li>
-				</ul>
-				<p id="newUser"></p>
-			</div>
-	
-	) 
-}
+	)
+	}
